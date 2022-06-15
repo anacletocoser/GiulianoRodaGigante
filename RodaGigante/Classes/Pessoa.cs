2602c4975d84d68776d5c9adfd76a0f95331fbec
Anacleto De Angeli Coser <anacleto.coser@gmail.com>
2022-06-15 20:43:30 -0300

    Commit inicial 2.0

---
+++ RodaGigante/Classes/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RodaGigante
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}

f4652e79fd44f906211869b9c52049ec3df3a673
Anacleto De Angeli Coser <anacleto.coser@gmail.com>
2022-06-15 19:21:12 -0300

    Commit Inicial

---
+++ RodaGigante/Classes/Criança.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RodaGigante
+{
+    public class Crianca : Pessoa
+    {
+        public Adulto Responsavel { get; set; }
+
+        public Crianca(string nome, int idade, Adulto responsavel) : base(nome, idade)
+        {
+            Nome = nome;
+            Idade = idade;
+            Responsavel = responsavel;
+        }
+
+        public Crianca(string nome, int idade) : base(nome, idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}
